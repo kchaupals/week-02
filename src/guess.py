112e4ef4c716fadf2268f00f4d9d325a8189c845
Kārlis Čaupals <74049486+kchaupals@users.noreply.github.com>
2026-02-15 12:22:18 +0200

    fix: Guess game - only accepts 10 tries and also returns how many tries are left

--- src/guess.py
+++ src/guess.py
@@ -1,21 +1,28 @@
 # Guess game with random number generation
 import random
+def guess_game():
+    guessNumber = random.randint(1, 100)
+    attempts = 0
+    maxAttempts = 10
+    print("Sveicināti spēle 'Uzmini skaitli'")
+    print("Es esmu izvēlējies skaitli no 1 līdz 100. Vai vari to uzminēt 10 mēģinājumos?")
+    while attempts < maxAttempts:
+        try:
+            user_guess = int(input("Ievadi savu minējumu: "))
+            attempts += 1
+            attemptsLeft = maxAttempts - attempts
+            if user_guess < guessNumber and attempts < 10:
+                print(f"Par mazu! Mēģini vēlreiz. Tev ir atlikuši {attemptsLeft} mēģinājums(-i)!")
+            elif user_guess > guessNumber and attempts < 10:
+                print(f"Par lielu! Mēģini vēlreiz. Tev ir atlikuši {attemptsLeft} mēģinājums(-i)!")
+            elif user_guess == guessNumber and attempts != maxAttempts:
+                print(f"Super, spēle beigusies! Tu uzminēji skaitli {guessNumber} pēc {attempts} mēģinājumiem!")
+                break
+            else:
+                print(f"Tev neizdevās uzminēt. Atbilde bija {guessNumber}")
+                break
+        except ValueError:
+            print("Nederīga ievade. Lūdzu, ievadi skaitli no 1 līdz 100.")
 
-guessNumber = random.randint(1, 100)
-attempts = 0
-print("Sveicināti spēle 'Uzmini skaitli'")
-print("Es esmu izvēlējies skaitli no 1 līdz 100. Vai vari to uzminēt?")
-while True:
-    try:
-        user_guess = int(input("Ievadi savu minējumu: "))
-        attempts += 1
-        if user_guess < guessNumber:
-            print("Par mazu! Mēģini vēlreiz.")
-        elif user_guess > guessNumber:
-            print("Par lielu! Mēģini vēlreiz.")
-        else:
-            print(f"Super, pēle beigusies! Tu uzminēji skaitli {guessNumber} pēc {attempts} mēģinājumiem!")
-            print("Vai velētos spēlēt vēlreiz? (jā/nē)")
-            break
-    except ValueError:
-        print("Nederīga ievade. Lūdzu, ievadi skaitli no 1 līdz 100.")
+guess_game()
+     
